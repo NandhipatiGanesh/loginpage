2a7077bccb1dc00d60213f3a6159ae227036e6ce
Ganesh <126049941+NandhipatiGanesh@users.noreply.github.com>
2025-05-10 16:51:41 +0530

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Karla } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+const karla = Karla({ variable: "--font-karla", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OTP Loginauth",
-  description: "OTP SINGUP LOGIN PAGE",
+  description: "OTP SIGNUP LOGIN PAGE",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${karla.variable} antialiased`}
       >
         {children}
       </body>
